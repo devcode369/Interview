67dd8d62e18ab51fe6f68a997ca306f77316f4fe
sathish kumar <devsathish05@outlook.com>
2022-09-05 19:14:17 +0530

    1131_Maximum of Absolute Value Expression

--- DSAP/ArraysMaths.cs
+++ DSAP/ArraysMaths.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -61,25 +62,62 @@ namespace DSAP
             }
             int end = arrayLen - 1;
             int start = 0, waterTrapped = 0, maxLeft = height[start], maxRight = height[end];
-            
+
             while (start < end)
             {
-                if (maxLeft<maxRight)
+                if (maxLeft < maxRight)
                 {
-                   start++;
-                    maxLeft=Math.Max(maxLeft, height[start]);
-                    waterTrapped+=maxLeft- height[start];
+                    start++;
+                    maxLeft = Math.Max(maxLeft, height[start]);
+                    waterTrapped += maxLeft - height[start];
 
                 }
                 else
                 {
                     end--;
-                    maxRight=Math.Max(maxRight, height[end]);
-                    waterTrapped+=maxRight- height[end];
+                    maxRight = Math.Max(maxRight, height[end]);
+                    waterTrapped += maxRight - height[end];
                 }
             }
 
             return waterTrapped;
         }
+
+
+        //1131. Maximum of Absolute Value Expression
+        //https://leetcode.com/problems/maximum-of-absolute-value-expression/
+        public static int MaxAbsValExpr(int[] arr1, int[] arr2)
+        {
+            int arr1Len = arr1.Length;
+            int arr2Len = arr2.Length;
+            int result = 0;
+            int[] arr3 = new int[arr1Len];
+            int[] arr4 = new int[arr1Len];
+            int[] arr5 = new int[arr1Len];
+            int[] arr6 = new int[arr1Len];
+            int[] arr7 = new int[4];
+
+            if (arr1 == null || arr2 == null || arr1Len == 0 || arr2Len == 0 || arr1Len != arr2Len)
+            {
+                return result;
+            }
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr3.SetValue((i + arr1[i] + arr2[i]), i);
+                arr4.SetValue((i - arr1[i] + arr2[i]), i);
+                arr5.SetValue((i + arr1[i] - arr2[i]), i);
+                arr6.SetValue((i - arr1[i] - arr2[i]), i);
+            }
+
+            arr7.SetValue(Math.Max(result, arr3.Max() - arr3.Min()), 0);
+            arr7.SetValue(Math.Max(result, arr4.Max() - arr4.Min()), 1);
+            arr7.SetValue(Math.Max(result, arr5.Max() - arr5.Min()), 2);
+            arr7.SetValue(Math.Max(result, arr6.Max() - arr6.Min()), 3);
+
+            return arr7.Max();
+
+        }
+
     }
 }
--- DSAP/Program.cs
+++ DSAP/Program.cs
@@ -12,8 +12,12 @@ namespace DSAP
         {
             var result = ArraysMaths.Trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 });
             var result1 = ArraysMaths.Trap2(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 });
+
+          //  var result3 = ArraysMaths.MaxAbsValExpr(new[] {1, 2, 3, 4 }, new[] { -1, 4, 5, 6 } );
+            var result3 = ArraysMaths.MaxAbsValExpr(new[] { 1, -2, -5, 0, 10 }, new[] { 0, -2, -1, -7, -4 });
             Console.WriteLine(result);
             Console.WriteLine(result1);
+            Console.WriteLine(result3);
             Console.ReadKey();
         }
     }
