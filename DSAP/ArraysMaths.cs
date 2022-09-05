35303a04b8eb451ec9a281fb5aad28a633fb0f06
sathish kumar <devsathish05@outlook.com>
2022-09-05 13:18:12 +0530

    42_TrappingRainWater

--- DSAP/ArraysMaths.cs
+++ DSAP/ArraysMaths.cs
@@ -6,7 +6,80 @@ using System.Threading.Tasks;
 
 namespace DSAP
 {
-    public class ArraysMaths
+    public static class ArraysMaths
     {
+
+        //42. Trapping Rain Water
+        //https://leetcode.com/problems/trapping-rain-water/
+
+        public static int Trap(int[] height)
+        {
+            int arrayLen = height.Length;
+            if (height == null || arrayLen == 0)
+            {
+                return 0;
+            }
+
+            int start = 0, waterTrap = 0, maxLeft = 0, maxRight = 0;
+            int end = arrayLen - 1;
+            while (start < end)
+            {
+                if (height[start] < height[end])
+                {
+                    if (height[start] >= maxLeft)
+                    {
+                        maxLeft = height[start];
+                    }
+                    else
+                    {
+                        waterTrap += maxLeft - height[start];
+                    }
+                    start++;
+                }
+                else
+                {
+                    if (height[end] >= maxRight)
+                    {
+                        maxRight = height[end];
+                    }
+                    else
+                    {
+                        waterTrap += maxRight - height[end];
+                    }
+                    end--;
+                }
+            }
+
+            return waterTrap;
+        }
+        public static int Trap2(int[] height)
+        {
+            int arrayLen = height.Length;
+            if (height == null || arrayLen == 0)
+            {
+                return 0;
+            }
+            int end = arrayLen - 1;
+            int start = 0, waterTrapped = 0, maxLeft = height[start], maxRight = height[end];
+            
+            while (start < end)
+            {
+                if (maxLeft<maxRight)
+                {
+                   start++;
+                    maxLeft=Math.Max(maxLeft, height[start]);
+                    waterTrapped+=maxLeft- height[start];
+
+                }
+                else
+                {
+                    end--;
+                    maxRight=Math.Max(maxRight, height[end]);
+                    waterTrapped+=maxRight- height[end];
+                }
+            }
+
+            return waterTrapped;
+        }
     }
 }
--- DSAP/Program.cs
+++ DSAP/Program.cs
@@ -10,6 +10,11 @@ namespace DSAP
     {
         static void Main(string[] args)
         {
+            var result = ArraysMaths.Trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 });
+            var result1 = ArraysMaths.Trap2(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 });
+            Console.WriteLine(result);
+            Console.WriteLine(result1);
+            Console.ReadKey();
         }
     }
 }
