7c027aa92a472c2a57c58e50a1669f50a7cbcb0e
sathish kumar <devsathish05@outlook.com>
2022-09-09 18:45:13 +0530

    Arrays&maths09

--- DSAP/Arrays.cs
+++ DSAP/Arrays.cs
@@ -12,8 +12,44 @@ namespace DSAP
         public static void Ge2DArrays()
         {
 
-            int[,] theArray = new int[5, 10];
-            Console.WriteLine("The array has {0} dimensions.", theArray.Rank);
+            string[,] theArray = new string[5, 10];
+            string[,,,] theArrayMulti = new string[5,5,5,5];
+
+            for (int i = 0; i < theArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < theArray.GetLength(1); j++)
+                {
+                    theArray[i, j] = "sathish" + i + "," + j;
+                }
+            }
+
+            foreach (var row in theArray)
+            {
+                foreach (var colm in row)
+                {
+                    Console.WriteLine(String.Format("{0} ", colm));
+                
+                }
+                Console.Write("\n");
+            }
+            for (int i = 0; i < theArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < theArray.GetLength(1); j++)
+                {
+                    Console.Write(string.Format("{0} ", theArray[i, j]));
+                }
+                Console.Write("\n");
+            }
+            // Console.WriteLine("The array has {0} dimensions.", theArray.Rank);
+
+
+
+
+        
+           
+
         }
+
+
     }
 }
--- DSAP/ArraysMaths.cs
+++ DSAP/ArraysMaths.cs
@@ -274,5 +274,158 @@ namespace DSAP
             }
             return currentMax;
         }
+
+        //73. Set Matrix Zeroes
+        public static void SetZeroes1(int[][] matrix)
+        {
+            Boolean isCol = false;
+            int R = matrix.Length;
+            int C = matrix[0].Length;
+
+            for (int i = 0; i < R; i++)
+            {
+
+                // Since first cell for both first row and first column is the same i.e. matrix[0][0]
+                // We can use an additional variable for either the first row/column.
+                // For this solution we are using an additional variable for the first column
+                // and using matrix[0][0] for the first row.
+                if (matrix[i][0] == 0)
+                {
+                    isCol = true;
+                }
+
+                for (int j = 1; j < C; j++)
+                {
+                    // If an element is zero, we set the first element of the corresponding row and column to 0
+                    if (matrix[i][j] == 0)
+                    {
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
+                }
+            }
+
+            // Iterate over the array once again and using the first row and first column, update the elements.
+            for (int i = 1; i < R; i++)
+            {
+                for (int j = 1; j < C; j++)
+                {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+
+            // See if the first row needs to be set to zero as well
+            if (matrix[0][0] == 0)
+            {
+                for (int j = 0; j < C; j++)
+                {
+                    matrix[0][j] = 0;
+                }
+            }
+
+            // See if the first column needs to be set to zero as well
+            if (isCol)
+            {
+                for (int i = 0; i < R; i++)
+                {
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        public static void SetZeroes(int[][] matrix)
+        {
+            bool isCol = false;
+            int row = matrix.Length;
+            int column = matrix[0].Length;
+
+            for (int i = 0; i < row; i++)
+            {
+                if (matrix[i][0] == 0)
+                {
+                    isCol = true;
+                }
+
+                for (int j = 1; j < column; j++)
+                {
+                  
+                    if (matrix[i][j] == 0)
+                    {
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
+                }
+            }
+     
+            for (int i = 1; i < row; i++)
+            {
+                for (int j = 1; j < column; j++)
+                {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+          
+            if (matrix[0][0] == 0)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    matrix[0][j] = 0;
+                }
+            }
+
+            if (isCol)
+            {
+                for (int i = 0; i < row; i++)
+                {
+                    matrix[i][0] = 0;
+                }
+            }
+
+            
+        }
+
+        //31. Next Permutation
+        //Single Pass Approach
+        public static void NextPermutation(int[] nums)
+        {
+            int i = nums.Length - 2;
+            while (i >= 0 && nums[i + 1] <= nums[i])
+            {
+                i--;
+            }
+            if (i >= 0)
+            {
+                int j = nums.Length - 1;
+                while (nums[j] <= nums[i])
+                {
+                    j--;
+                }
+                Swap(nums, i, j);
+            }
+            Reverse(nums, i + 1);
+        }
+        private static void Reverse(int[] nums, int start)
+        {
+            int i = start, j = nums.Length - 1;
+            while (i < j)
+            {
+                Swap(nums, i, j);
+                i++;
+                j--;
+            }
+        }
+
+        private static void Swap(int[] nums, int i, int j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
     }
 }
